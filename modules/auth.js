67e1ccf3c5b3fdcdc59efbce044a87b4b497f642
NowUooMe <andr-1088@mail.ru>
2017-07-21 11:38:18 +0300

    Added authentication
    Also changed router for better auth mekanism, added function for safe request, first commit of register.html, changed and added some functions in db and tech_server

--- modules/auth.js
+++ modules/auth.js
@@ -1,14 +1,87 @@
 var db = require('db');
-var config = require('config');
-var keccak = require('js-sha3').keccak512;
+var log = require('tech').log;
+var extra = require('./extra');
 
-/**
- * In developing
- */
-exports.getHash = function(res, login, hash) {
-    var user = db.getUser(login);
-    if (hash == keccak(user.login + user.password + config.SECRET_KEY))
-        res.end("chat");
-    else
-        res.end("decline");
+exports.session = function(req, res) {
+    var ID = extra.parseCookies(req).sessionID;
+
+    db.sessions.getSession(ID)
+        .then(function (data) {
+            if (data) {
+                res.writeHead(302, { Location: req.url + 'chat'});
+                res.end();
+
+            } else {
+                res.writeHead(302, { Location: req.url + 'auth'});
+                res.end();
+            }
+        })
+        .catch(function (err) {
+            console.log("Error in auth.js/getSession", err);
+        });
+};
+
+exports.auth = function(req, res) {
+    extra.safeRequest(req, res)
+        .then(function(body) {
+            db.users.getUserWrite(body.login)
+                .then(function (data) {
+
+                    if (data) {
+                        
+                        if (data.password == body.password) {
+                            db.sessions.addSession(body.login,
+                                new Date().getTime() + 86409000)
+                                .then(function(data) {
+
+                                    var s = 'sessionID='
+                                        + data + '; expires=' +
+                                        (new Date(new Date().getTime() + 86409000))
+                                            .toUTCString() + '; Path=/; HttpOnly';
+
+                                    res.writeHead(200, {
+                                        'Set-Cookie': s
+                                    });
+                                    res.end('/chat');
+
+                                })
+                                .catch(function (err) {
+                                    log.error("Error at auth.js/auth:", err);
+                                });
+                        } else {
+                            res.statusCode = 401;
+                            res.end();
+
+                        }
+                    }
+                })
+                .catch(function (err) {
+                    log.error("Error at auth.js/auth:", err);
+                });
+        })
+        .catch(function(err) {
+            log.error(err);
+        });
+};
+
+exports.salt = function(req, res) {
+    extra.safeRequest(req, res)
+        .then(function(data) {
+            var login = data.login;
+
+            db.users.getUserRead(login)
+                .then(function(data) {
+                    if (data) {
+                        res.end(data.salt)
+                    } else {
+                        res.end(data);
+                    }
+                })
+                .catch(function(err) {
+                    log.error("Error at auth.js/salt:", err);
+                });
+        })
+        .catch(function(err) {
+            log.error(err);
+        });
 };
